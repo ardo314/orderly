b152594cdee6081a0181704c0be66a32e8ce32a5
Leonard Hansing <lhansing92@gmail.com>
2024-07-01 20:47:56 +0200

    Added some styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,25 @@
 import { useEffect, useRef, useState } from 'react'
 import './App.css'
 import { core } from "game"
+import { shuffle } from './utils';
+
+const colors = shuffle([
+  "red",
+  "green",
+  "blue",
+  "yellow",
+  "azure",
+  "orange",
+  "purple",
+  "brown",
+  "black",
+  "grey",
+]);
 
 function App() {
   const gameRef = useRef(new core.Game());
   const [numbers, setNumbers] = useState<number[]>([])
-  
+
   function game() {
     return gameRef.current;
   }
@@ -16,7 +30,9 @@ function App() {
 
   return (
     <>
-      {numbers.map((number, index) => (<div key={index}>{number}</div>))}
+      <div className="grid">
+        {numbers.map((_, index) => (<div key={index} className='grid-item' style={{ backgroundColor: colors[index] }} />))}
+      </div>
     </>
   )
 }
